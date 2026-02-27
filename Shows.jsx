82647f7e0e8c68c06a329c64ab57317e1e76c19b
DLokiv18 <dhanamjayalokesh23@gmail.com>
2026-02-27 23:09:22 +0530

    Fixed ShowBanner import

--- Shows.jsx
+++ Shows.jsx
@@ -1,4 +1,4 @@
-import Showbanner from "./ShowBanner"
+import Showbanner from "./Showbanner"
 import React,{useState,useEffect} from "react"
 import { useNavigate } from "react-router-dom"
 function Shows(){
